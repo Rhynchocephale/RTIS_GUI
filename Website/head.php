235380dfbaa436967dcaac1c7af16c34baedd0b0
cdrouadaine <clement.drouadaine@orange.fr>
2015-05-27 12:56:01 +0200

    removing useless files

--- Website/head.php
+++ Website/head.php
@@ -16,12 +16,12 @@
 	<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
 
 	<!-- you need to include the shieldui css and js assets in order for the charts to work -->
-	<link rel="stylesheet" type="text/css" href="http://www.shieldui.com/shared/components/latest/css/shieldui-all.min.css" />
+	<!-- <link rel="stylesheet" type="text/css" href="http://www.shieldui.com/shared/components/latest/css/shieldui-all.min.css" />
 	<link rel="stylesheet" type="text/css" href="http://www.shieldui.com/shared/components/latest/css/light-bootstrap/all.min.css" />
 	<link id="gridcss" rel="stylesheet" type="text/css" href="http://www.shieldui.com/shared/components/latest/css/dark-bootstrap/all.min.css" /> 
 
 	<script type="text/javascript" src="http://www.shieldui.com/shared/components/latest/js/shieldui-all.min.js"></script>
-	<script type="text/javascript" src="http://www.prepbootstrap.com/Content/js/gridData.js"></script>
+	<script type="text/javascript" src="http://www.prepbootstrap.com/Content/js/gridData.js"></script> -->
 	
 	<script type="text/javascript" src="js/custom.js"></script>
 </head>
