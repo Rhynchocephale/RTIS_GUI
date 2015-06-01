24fcaa778dde49b95158cf2a9802aa83d353e8fe
cdrouadaine <clement.drouadaine@orange.fr>
2015-06-01 08:21:05 +0200

    Putting in a real config file

--- Website/config.php
+++ Website/config.php
@@ -5,6 +5,15 @@
 	connectToDb();
 ?> 
 
+<div class="row">
+	<div class="col-lg-12">
+		<div class="alert alert-dismissable alert-warning">
+			<button type="button" class="close" data-dismiss="alert">&times;</button>
+			The changes you make here will not take effect until the next reboot.
+		</div>
+	</div>
+</div>
+
 <div class="panel panel-primary">
 	
 	<div class="panel-heading">
@@ -43,76 +52,260 @@
 		</div>
 		
 		<form role="form" method="post" action="submitConfig.php">
-			<div class="col-lg-6">
+			<div class="col-lg-4">
+				<div class="panel panel-default">
+					<div class="panel-heading">RTIM Configuration file</div>
+					<div class="panel-footer">
 				
-				<div class="form-group">
-					<label>Write stuff below</label>
-					<input id="name" class="form-control">
-				</div>
+						<div class="form-group">
+							<label>SeverityFilter</label>
+							<select class="form-control">
+								<option value="0">No error message</option>
+                                <option value="1">Fatal</option>
+                                <option value="3">Error</option>
+                                <option value="7">Warning</option>
+                                <option value="15">Info</option>
+                                <option value="31">All</option>
+                                <option value="159">Debug</option>
+                            </select>
+						</div>
 
-				<div class="form-group">
-					<label>Write more stuff here</label>
-					<input class="form-control">
+						<div class="form-group">
+							<label>StationId</label>
+							<input class="form-control">
+						</div>
+						
+						<div class="form-group">
+							<label>RxIP_Address</label>
+							<input class="form-control">
+						</div>
+						
+						<div class="form-group">
+							<label>RxPortNo</label>
+							<input class="form-control">
+						</div>
+						
+						<div class="form-group">
+							<label>RxSocketType</label>
+							<input class="form-control">
+						</div>
+						
+						<label>RxIOTimeout</label>
+						<div class="form-group input-group">
+							<input type="text" class="form-control">
+                            <span class="input-group-addon">sec</span>
+						</div>
+						
+						<label>RxConnectionTimeout</label>
+						<div class="form-group input-group">
+							<input type="text" class="form-control">
+                            <span class="input-group-addon">sec</span>
+						</div>
+						
+						<label>RxRetryDelay</label>
+						<div class="form-group input-group">
+							<input type="text" class="form-control">
+                            <span class="input-group-addon">sec</span>
+						</div>
+						
+						<div class="form-group">
+							<label>StationShortName</label>
+							<input class="form-control">
+						</div>
+						
+						<div class="form-group">
+							<label>ReceiverPosition</label>
+							<input class="form-control">
+						</div>				
+					</div>
 				</div>
-				
-				<div class="form-group">
-					<label>Stuff to check</label>
-					<label class="checkbox-inline">
-						<input type="checkbox">
-						1
-					</label>
-					<label class="checkbox-inline">
-						<input type="checkbox">
-						2
-					</label>
-					<label class="checkbox-inline">
-						<input type="checkbox">
-						3
-					</label>
-				</div>
-				
 			</div>
-			<div class="col-lg-6">
-				
-				<div class="form-group">
-					<label>Write stuff in another column</label>
-					<input class="form-control">
-				</div>
-
-				<div class="form-group">
-					<label>Check one of these stuff</label>
-					<label class="radio-inline">
-						<input type="radio" name="optionsRadiosInline" id="optionsRadiosInline1" value="option1" checked>
-						1
-					</label>
-					<label class="radio-inline">
-						<input type="radio" name="optionsRadiosInline" id="optionsRadiosInline2" value="option2">
-						2
-					</label>
-					<label class="radio-inline">
-						<input type="radio" name="optionsRadiosInline" id="optionsRadiosInline3" value="option3">
-						3
-					</label>
-				</div>
-
-				<div class="form-group">
-					<label>Select stuff</label>
-					<select id="number" class="form-control">
-						<option value="0">0</option>
-						<option value="1">1</option>
-						<option value="2">2</option>
-						<option value="3">3</option>
-						<option value="4">4</option>
-						<option value="5">5</option>
-					</select>
+			
+			<div class="col-lg-4">
+				<div class="panel panel-default">
+					<div class="panel-heading">GNSS Receiver Command Server Module</div>
+					<div class="panel-footer">
+						<div class="form-group">
+							<label>SeverityFilter</label>
+							<select class="form-control">
+								<option value="0">No error message</option>
+                                <option value="1">Fatal</option>
+                                <option value="3">Error</option>
+                                <option value="7">Warning</option>
+                                <option value="15">Info</option>
+                                <option value="31">All</option>
+                                <option value="159">Debug</option>
+                            </select>
+						</div>
+					</div>
 				</div>
 				
+				<div class="panel panel-default">
+					<div class="panel-heading">GNSS RawData Server Module</div>
+					<div class="panel-footer">
+						<div class="form-group">
+							<label>SeverityFilter</label>
+							<select class="form-control">
+								<option value="0">No error message</option>
+                                <option value="1">Fatal</option>
+                                <option value="3">Error</option>
+                                <option value="7">Warning</option>
+                                <option value="15">Info</option>
+                                <option value="31">All</option>
+                                <option value="159">Debug</option>
+                            </select>
+						</div>
+						
+						<label>SampleRate</label>
+						<div class="form-group input-group">
+                            <select class="form-control">
+								<option value="10">10</option>
+                                <option value="20">20</option>
+                                <option value="40">40</option>
+                                <option value="50">50</option>
+                                <option value="100">100</option>
+                                <option value="200">200</option>
+                                <option value="500">500</option>
+                                <option value="1000">1000</option>
+                            </select>
+                            <span class="input-group-addon">msec</span>
+                        </div>
+
+					</div>
+				</div>
+				
+				<div class="panel panel-default">
+					<div class="panel-heading">GNSS Ephemerides Server Module</div>
+					<div class="panel-footer">
+						<div class="form-group">
+							<label>SeverityFilter</label>
+							<select class="form-control">
+								<option value="0">No error message</option>
+                                <option value="1">Fatal</option>
+                                <option value="3">Error</option>
+                                <option value="7">Warning</option>
+                                <option value="15">Info</option>
+                                <option value="31">All</option>
+                                <option value="159">Debug</option>
+                            </select>
+						</div>
+					</div>
+				</div>
+				
+				<div class="panel panel-default">
+					<div class="panel-heading">Processing</div>
+					<div class="panel-footer">
+			
+						<div class="form-group">
+							<label>SeverityFilter</label>
+							<select class="form-control">
+								<option value="0">No error message</option>
+                                <option value="1">Fatal</option>
+                                <option value="3">Error</option>
+                                <option value="7">Warning</option>
+                                <option value="15">Info</option>
+                                <option value="31">All</option>
+                                <option value="159">Debug</option>
+                            </select>
+						</div>
+						
+						<div class="form-group">
+							<label>DopplerTolerance</label>
+							<input class="form-control">
+						</div>
+						
+						<div class="form-group">
+							<label>FilterFreq</label>
+							<input class="form-control">
+						</div>
+					</div>
+				</div>
 			</div>
-	
-			<button name="save" type="button" class="btn btn-primary">Save</button>
-			<button name="saveSend" type="button" class="btn btn-success">Save & send</button>
-			<button name="send" type="button" class="btn btn-warning" data-toggle="modal" data-target="#confirmNoSave">Send (but don't save)</button>
-		
+			
+			<div class="col-lg-4">
+				<div class="panel panel-default">
+					<div class="panel-heading">Index Client Module</div>
+					<div class="panel-footer">
+				
+						<div class="form-group">
+							<label>SeverityFilter</label>
+							<select class="form-control">
+								<option value="0">No error message</option>
+                                <option value="1">Fatal</option>
+                                <option value="3">Error</option>
+                                <option value="7">Warning</option>
+                                <option value="15">Info</option>
+                                <option value="31">All</option>
+                                <option value="159">Debug</option>
+                            </select>
+						</div>
+						
+						<div class="form-group">
+							<label>TxIP_Address</label>
+							<input class="form-control">
+						</div>
+						
+						<div class="form-group">
+							<label>TxPortNo</label>
+							<input class="form-control">
+						</div>
+						
+						<div class="form-group">
+							<label>TxSocketType</label>
+							<input class="form-control">
+						</div>
+						
+						<label>TxIOTimeout</label>
+						<div class="form-group input-group">
+							<input type="text" class="form-control">
+                            <span class="input-group-addon">sec</span>
+						</div>
+						
+						<label>TxConnectionTimeout</label>
+						<div class="form-group input-group">
+							<input type="text" class="form-control">
+                            <span class="input-group-addon">sec</span>
+						</div>
+
+						<label>TxRetryDelay</label>
+						<div class="form-group input-group">
+							<input type="text" class="form-control">
+                            <span class="input-group-addon">sec</span>
+						</div>
+					</div>
+				</div>
+					
+				<div class="panel panel-default">
+					<div class="panel-heading">Output</div>
+					<div class="panel-footer">
+			
+						<div class="form-group">
+							<label>SeverityFilter</label>
+							<select class="form-control">
+								<option value="0">No error message</option>
+                                <option value="1">Fatal</option>
+                                <option value="3">Error</option>
+                                <option value="7">Warning</option>
+                                <option value="15">Info</option>
+                                <option value="31">All</option>
+                                <option value="159">Debug</option>
+                            </select>
+						</div>
+						
+						<div class="form-group">
+							<label>RootDirectory</label>
+							<input class="form-control">
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div class="pull-right">
+				<button name="save" type="button" class="btn btn-primary">Save</button>
+				<button name="saveSend" type="button" class="btn btn-success">Save & send</button>
+				<button name="send" type="button" class="btn btn-warning" data-toggle="modal" data-target="#confirmNoSave">Send (but don't save)</button>
+			</div>
+			
 			<div class="modal fade" id="confirmNoSave" tabindex="-1" role="dialog" aria-labelledby="confirmNoSave" aria-hidden="true">
 				<div class="modal-dialog">
 					<div class="modal-content">
@@ -152,9 +345,11 @@
 				
 			</div>
 			
-			<button name="saveFile" type="button" class="btn btn-primary">Save</button>
-			<button name="saveSendFile" type="button" class="btn btn-success">Save & send</button>
-			<button name="sendFile" type="button" class="btn btn-warning">Send (but don't save)</button>
+			<div class="pull-right">
+				<button name="save" type="button" class="btn btn-primary">Save</button>
+				<button name="saveSend" type="button" class="btn btn-success">Save & send</button>
+				<button name="send" type="button" class="btn btn-warning" data-toggle="modal" data-target="#confirmNoSave">Send (but don't save)</button>
+			</div>
 		</form>
 	</div>
 </div>
